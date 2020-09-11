2dab138562fdbc45461f558223bd9fe93bec0140
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-09-11 15:29:00 -0700

    Lab 4 Update
    Add Picture of CSS, Index Page Code, Lab 4 home page, and Lab javascript.
    Modified Index.html, Lab.js, and lab.css for lab 4. Modfied Site.css as well.

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+//Make several maps using a free to use game engine.
+  //Sending it to the team for assessment and troubleshooting.
+  //Create several characters and assign keys to actions.
+  //Create the npcs and their auto functions based on character actions.
+  //Anticipate every possible scenario.
+       // IF x does why then Y does Z.
+  //Set win conditions and troubleshoot the full game part by part.
+  //Send Final Evaluation
+      //Fix errors
