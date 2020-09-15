dfe5ac8775becd8054ef7445235da5bd908e9581
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-09-14 18:52:43 -0700

    Lab 5 Update
    Add five picture to Lab 5 file. Modifed and changed Index.html and lab.js. Modified and added Id tags in Site.css

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Sean Brown, Ryen Rhebar
+* Created: September 14th, 2020
+* Public Domain
+*/
+//Variable
+var make = "Subaru";
+var model = "Outback";
+var color = "Bluish Grey";
+var year =2005;
+
+
+//Calculation
+  var age = 2020 - year;
+
+
+//Ouctome
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
