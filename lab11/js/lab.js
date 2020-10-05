631f73164bd6e5cb575c1087c92aa991ca12635a
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-10-05 14:00:47 -0700

    Lab 11 Update 1.2
    Slight human error of putting the button and input in the div output. starting up bonus tasks.

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,12 +7,10 @@
 
 //function
 function sortingHat (str) {
-
   len = str.length;
-  mod = len % 4;
+  mod = len %  4;
   if (mod ==0) {
     return "Gryffindor"
-
   }
   else if (mod ==1) {
     return "Ravenclaw"
@@ -23,14 +21,45 @@ function sortingHat (str) {
   else if (mod ==3) {
     return "Hufflepuff"
   }
-  //housesArray
-
-
 }
+//housesArray
+housesArray = [
+{
+title: "Gryffindor",
+text: " hello"
+},
+
+{
+title: "Ravenclaw",
+text: " hello"
+},
+
+{
+title: "Slytherin",
+text: " hello"
+
+},
+
+{
+title: "Hufflepuff",
+text: " hello"
+},
+
+]
+
+
+function sortingHatFinal(str) {
+  mod = Math.abs(4) % housesArray.length;
+  return housesArray[mod];
+}
+
 var myButton = document.getElementById ("button");
 myButton.addEventListener ("click", function () {
     var name = document.getElementById("input").value;
-    var house = sortingHat(name);
-    newText = "<b><p> The Sorting Hat has sorted you into " + house +".</p></b>";
+    var house = sortingHatFinal(name);
+
+    newText = "<b><p> The Sorting Hat has sorted you into " + house.title +".</p></b>" +
+     "<p>" + house.text + "</p>";
+
     document.getElementById("output").innerHTML = newText;
 })
