06497897675402338ae89663f129afd116cd74da
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-10-20 11:22:08 -0700

    Lab 14 update 1.2
    modified and add code.

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -3,50 +3,22 @@
 * Created: September 14th, 2020
 * Public Domain
 */
+//Function
+function Vehicle(brand, name, date, color, extras) {
+this.brand = brand;
+this.name = name;
+this.date = date;
+this.color = color;
+this.extras= extras;
+this.info= function () {
+    return(this.brand +", "+this.name+", "+this.date+ ", "+this.color+", "+this.extras+ ".");
+};
+};
+
 //Variables
-var str = "";
-  var i=0;
-
-  //For function
-for (i =1; i <= 200; i++) {
-  //print out the number, the basic part of the list.
-  str+= i +". - ";
-if(i%35==0) {
-str +=   " BuzzBoom!<br>";
-console.log ("FizzBoom");
-}
-    //adds Fizz,Buzz, Boom, Etc...
-    else if (i%21==0) {
-    str +=   " FizzBoom!<br>";
-  console.log ("FizzBoom");
-}
- else  if (i%15 == 0)
-  { str +=  " FizzBuzz! <br>";
-  console.log ("FizzBuzz");
-  }
-  else if (i %3 ==0) {
-  str +=   " Fizz!<br>";
-    console.log ("Fizz");
-  }
-  else if (i %5 ==0) {
-  str +=   " Buzz!<br>";
-    console.log("Buzz");
-  }
-  else if (i%7 ==0) {
-  str +=   " Boom!<br>";
-    console.log("Boom");
-  }
-// if there is no assigned number, it will just fill in via the assigned number with a break.
-  else {
-  str+= i + "</br>";
-        console.log(i);
-  }
-}
-
-
-//BONUS
+var bike = new Vehicle("Schwinn", "LeToure", 1974, "gold", "Brooks saddle");
 
 
 
 //output
- document.getElementById("output").innerHTML = str;
+document.getElementById("output").innerHTML += "<p>" + bike.info() + "</p>";
