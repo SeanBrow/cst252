b2fb252c02352e60ac2fd19b8565cc0fa2cca818
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-10-27 18:58:11 -0700

    Lab 16 Update
    Add lab 16 and modified the home page.

---
+++ lab16/js/lab.js
@@ -0,0 +1,10 @@
+/*
+* Author: Sean Brown,
+* Created: September 14th, 2020
+* Public Domain
+*/
+//Function
+$("output").ready(function(){
+$(":button").css("background-color", "#22bb45").click(function(){
+  alert("This Button is working!.")});
+});
