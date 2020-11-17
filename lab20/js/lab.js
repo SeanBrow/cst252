bb9207ad092c1550b556f8884e95c4197b2c4338
SeanBrow <70295297+SeanBrow@users.noreply.github.com>
2020-11-16 16:09:49 -0800

    Lab 20 Update

---
+++ lab20/js/lab.js
@@ -0,0 +1,24 @@
+/*
+* Author: Sean Brown,
+* Created: November 16th, 2020
+* Public Domain
+*/
+
+URL="https://xkcd.com/info.0.json"
+  $.ajax({
+url: URL,
+data: {
+
+},
+type:"GET"
+})
+// If the request succeeds
+.done(function( data ) {
+  console.log(data);
+  var imageUrl= data.img;
+  var title = data.title;
+  var alt=data.alt;
+  var html= '<div id="output"> <h2> ${title}</h2> <img src="${imageUrl}" title="$alt"> </div>'
+  $("#result").append(html);
+
+  })
